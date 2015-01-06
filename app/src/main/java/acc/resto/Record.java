95d230032df9763c80a55049857ac9bc99481f1e
Swapnil Singh <sw4pn1l.s1ngh@gmail.com>
2015-01-06 11:30:36 +0530

    Removed unwanted notation
    new commit

--- app/src/main/java/acc/resto/Record.java
+++ app/src/main/java/acc/resto/Record.java
@@ -11,17 +11,11 @@ import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.model.GraphUser;
-<<<<<<< HEAD
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
-=======
 import com.parse.ParseException;
-import com.parse.ParseFacebookUtils;
-import com.parse.ParseUser;
 import com.parse.SignUpCallback;
->>>>>>> origin/master
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Record extends Activity{
@@ -33,10 +27,7 @@ public class Record extends Activity{
         if(session!=null && session.isOpened())
         {
             makeMeRequest();
-<<<<<<< HEAD
             startNextActivity();
-=======
->>>>>>> origin/master
         }
     }
     private void makeMeRequest()
@@ -46,28 +37,8 @@ public class Record extends Activity{
             public void onCompleted(GraphUser user, Response response) {
                 if(user!=null)
                 {
-<<<<<<< HEAD
                     JSONObject jsonObject = new JSONObject();
-                    try{
-                        jsonObject.put("facebook id",user.getId());
-                        jsonObject.put("Name",user.getName());
-
-                        if(user.getLocation().getProperty("name")!=null){
-                            jsonObject.put("location",(String) user.getLocation().getProperty("name"));
-                        }
-                        if (user.getProperty("email") != null) {
-                            jsonObject.put("email", user.getProperty("email"));
-                        }
-
-                        ParseUser currentUser = ParseUser.getCurrentUser();
-                        currentUser.put("profile",jsonObject);
-                        currentUser.saveInBackground();
-
-                    } catch (JSONException e) {
-=======
                     ParseUser parseUser = new ParseUser();
-    //                JSONObject jsonObject = new JSONObject();
-                    ParseUser currentUser = ParseUser.getCurrentUser();
 
                     try{
     //                    jsonObject.put("facebook id",user.getId());
@@ -90,15 +61,13 @@ public class Record extends Activity{
                         parseUser.signUpInBackground(new SignUpCallback() {
                             @Override
                             public void done(ParseException e) {
-                                if(e == null)
-                                {
+                                if (e == null) {
                                     startNextActivity();
                                 }
                             }
                         });
 
                     } catch (Exception e) {
->>>>>>> origin/master
                         Toast.makeText(getApplicationContext(), "Error Parsing Info", Toast.LENGTH_LONG).show();
                     }
 
